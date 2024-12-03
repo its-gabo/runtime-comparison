2c1f4fda50c6330aaa310c3adf97538cc4aa7938
Gabo <smolana.gabriel@gmail.com>
2024-12-03 23:40:44 +0100

    fix: node server not responding

--- packages/node-server/server.ts
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer(() => {
-  return new Response("Hello World", { status: 200 });
-});
-
-server.listen(3000, () => {
-  console.log("Node server running at http://localhost:3000/");
-});
